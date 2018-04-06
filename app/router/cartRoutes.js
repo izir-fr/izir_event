a740afa573764700ff04a64996b5aa079c5ea19e
jn-prod <nicolas.jouanno@gmail.com>
2018-04-06 14:38:35 +0200

    cart speration routes and controllers

--- app/router/cartRoutes.js
+++ app/router/cartRoutes.js
@@ -9,30 +9,30 @@ var Product = require('../models/product'),
 var cartCtrl = require('../controllers/cartController')
 
 // Get add to cart a product
-router.get('/', cartCtrl.getAddToCart )
+router.get('/', cartCtrl.getAllProduct )
 
 // Get reduce product cart quantity by one
-router.get('/add/:id', cartCtrl.getReduceByOne )
+router.get('/add/:id', cartCtrl.getAddToCart )
 
 // Get increase product cart quantity by one
-router.get('/reduce-by-one/:id', cartCtrl.getIncreaseByOne )
+router.get('/reduce-by-one/:id', cartCtrl.getReduceByOne )
 
 // Get reduce product cart quantity by X
-router.get('/increase-by-one/:id',  cartCtrl.getReduceByX )
+router.get('/increase-by-one/:id',  cartCtrl.getIncreaseByOne )
 
 // Get increase product cart quantity by X
-router.get('/reduce-by-x/:id', cartCtrl.getIncreaseByX )
+router.get('/reduce-by-x/:id', cartCtrl.getReduceByX )
 
 // Get remove a product in cart
-router.get('/increase-by-x/:id', cartCtrl.getRemoveProductCart )
+router.get('/increase-by-x/:id', cartCtrl.getIncreaseByX )
 
 // Get a product
-router.get('/remove/:id', cartCtrl.getProduct )
+router.get('/remove/:id', cartCtrl.getRemoveProductCart )
 
 // Get cart
-router.get('/produit/:id', cartCtrl.getCart )
+router.get('/produit/:id', cartCtrl.getProduct )
 
 // Get all product
-router.get('/panier', cartCtrl.getAllProduct )
+router.get('/panier', cartCtrl.getCart )
 
 module.exports = router;

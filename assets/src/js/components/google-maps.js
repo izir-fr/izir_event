1dbf1fa5edcf28a6599d8fffa643720aadf09b4e
jn-prod <nicolas.jouanno@gmail.com>
2018-05-09 13:09:02 +0200

    geolocate corr

--- assets/src/js/components/google-maps.js
+++ assets/src/js/components/google-maps.js
@@ -87,27 +87,29 @@ var googleMap = () => {
 
     // Bias the autocomplete object to the user's geographical location,
     // as supplied by the browser's 'navigator.geolocation' object.
-    // function geolocate () {
-    //   if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(function (position) {
-    //       var geolocation = {
-    //         lat: position.coords.latitude,
-    //         lng: position.coords.longitude
-    //       }
-    //       var circle = new google.maps.Circle({
-    //         center: geolocation,
-    //         radius: position.coords.accuracy
-    //       })
-    //       if (circle !== undefined) {
-    //         autocomplete.setBounds(circle.getBounds())
-    //       }
-    //     })
-    //   }
-    // }
+    function geolocate () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          // var geolocation = {
+          //   lat: position.coords.latitude,
+          //   lng: position.coords.longitude
+          // }
+          var geolocation = new google.maps.LatLng(
+            position.coords.latitude, position.coords.longitude)
+          var circle = new google.maps.Circle({
+            center: geolocation,
+            radius: position.coords.accuracy
+          })
+          autocomplete.setBounds(circle.getBounds())
+          // Log autocomplete bounds here
+          console.log(autocomplete.getBounds())
+        })
+      }
+    }
 
     initAutocomplete()
 
-    // geolocate()
+    geolocate()
   })
 }
 

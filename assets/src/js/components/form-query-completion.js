22003878c0001f03d0fc3da94ef87e1ea5d055eb
jn-prod <nicolas.jouanno@gmail.com>
2018-05-09 14:14:55 +0200

    document ready

--- assets/src/js/components/form-query-completion.js
+++ assets/src/js/components/form-query-completion.js
@@ -1,36 +1,36 @@
 var selectQuery = () => {
   $(() => {
-    var url = window.location.search.split('?')
-    if (url.length !== 0) {
-      console.log(url)
-      var allQueries = url[1].split('&')
-      var formatedQuery = []
+    $(document).ready(() => {
+      var url = window.location.search.split('?')
+      if (url.length !== 0) {
+        console.log(url)
+        var allQueries = url[1].split('&')
+        var formatedQuery = []
 
-      allQueries.forEach((val) => {
-        formatedQuery.push({
-          query: decodeURIComponent(val.split('=')[0]),
-          name: decodeURIComponent(val.split('=')[1])
+        allQueries.forEach((val) => {
+          formatedQuery.push({
+            query: decodeURIComponent(val.split('=')[0]),
+            name: decodeURIComponent(val.split('=')[1])
+          })
         })
-      })
 
-      console.log(allQueries)
-
-      $('select').each((key, input) => {
-        formatedQuery.forEach((val) => {
-          if (val.query === input.name) {
-            var options = $(input).children()
-            options.each((key, option) => {
-              var query = (val.name.split('+'))
-              var queryFormated = query.join(' ')
-              console.log(queryFormated)
-              if (option.value.toLowerCase() === queryFormated.toLowerCase()) {
-                option.selected = true
-              }
-            })
-          }
+        $('select').each((key, input) => {
+          formatedQuery.forEach((val) => {
+            if (val.query === input.name) {
+              var options = $(input).children()
+              options.each((key, option) => {
+                var query = (val.name.split('+'))
+                var queryFormated = query.join(' ')
+                console.log(queryFormated)
+                if (option.value.toLowerCase() === queryFormated.toLowerCase()) {
+                  option.selected = true
+                }
+              })
+            }
+          })
         })
-      })
-    }
+      }
+    })
   })
 }
 

510ef33b49202dac3512ec495d5dfdcaac456a23
jn-prod <nicolas.jouanno@gmail.com>
2018-05-09 15:30:55 +0200

    html script clean

--- assets/src/js/components/event-detail.js
+++ assets/src/js/components/event-detail.js
@@ -1,4 +1,13 @@
 var eventDetail = () => {
-  
+  $(() => {
+    if ($('input[name=dateClotureInscription]').length !== 0) {
+      var dateNow = new Date(Date.now())
+      var eventDate = new Date($('input[name=dateClotureInscription]').val())
+      if (dateNow > eventDate) {
+        $('#inscriptionButton').remove()
+        $('#date-info').text('Les inscriptions sont cloturées').addClass('badge-danger').removeClass('badge-warning')
+      }
+    }
+  })
 }
-export default eventDetail ()
+export default eventDetail()

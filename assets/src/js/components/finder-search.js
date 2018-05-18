9c25ea787edf9c6c80cd0667d24be8d433906c3f
jn-prod <nicolas.jouanno@gmail.com>
2018-05-18 14:41:17 +0200

    recap dasjboard display corr & finder mobile display corr

--- assets/src/js/components/finder-search.js
+++ assets/src/js/components/finder-search.js
@@ -4,6 +4,27 @@ var finder = () => {
       $($('.event')[key]).remove()
     }
   })
+  $(() => {
+    var finderSumbitButton = $('#finder-submit-button')
+    var finderButtonDesktop = $('#finder-button-desktop')
+    var finderButtonMobile = $('#finder-button-mobile')
+
+    var finderButton = () => {
+      if ($(window).innerWidth() <= 850) {
+        finderSumbitButton.remove()
+        finderButtonMobile.append(finderSumbitButton)
+      } else {
+        finderSumbitButton.remove()
+        finderButtonDesktop.append(finderSumbitButton)
+      }
+    }
+
+    finderButton()
+
+    $(window).on('resize', () => {
+      finderButton()
+    })
+  })
 }
 
 export default finder()

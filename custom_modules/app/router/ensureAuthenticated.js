8d7f098a9b7a74d2dc56489e94fc6777df452794
jn-prod <nicolas.jouanno@gmail.com>
2018-06-29 10:42:26 +0200

    redirect flow to inscription after login

--- custom_modules/app/router/ensureAuthenticated.js
+++ custom_modules/app/router/ensureAuthenticated.js
@@ -2,8 +2,12 @@ var ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next()
   } else {
-    // req.flash('error_msg','You are not logged in');
-    res.redirect('/user/login')
+    if (req.query.event_id) {
+      res.redirect('/user/login?event_id=' + req.query.event_id)
+    } else {
+      // req.flash('error_msg','You are not logged in');
+      res.redirect('/user/login')
+    }
   }
 }
 

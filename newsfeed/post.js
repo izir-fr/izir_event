bcae6f762cbeb73c56f010288c73973218d4e1df
jn-prod <nicolas.jouanno@gmail.com>
2018-11-22 11:39:41 +0100

    newsfeed update

--- newsfeed/post.js
+++ newsfeed/post.js
@@ -2,11 +2,15 @@ module.exports = [
   {
     titre: 'Gaël Le Bellec - Zofingen 2018 3rd World title',
     published_date: '21/11/2018',
-    youtube_id: 'psgP_DFQTq4'
+    youtube_id: 'psgP_DFQTq4',
+    long_text: ''
   }, {
     titre: 'Trail de pontivy - 2019',
     published_date: '19/11/2018',
     youtube_id: 'yroDl4qPrHk',
-    event_id: '5bcb08d4c8478f0023cc0cf1'
+    event_id: '5bcb08d4c8478f0023cc0cf1',
+    short_text: 'Le club "Athlétisme Pays de Pontivy" organise, le 25 novembre prochain, une nouvelle édition du Trail du Resto, à Pontivy. L\'épreuve est également la dernière étape du Triskell Challenge Trail.',
+    long_text: 'Les différents circuits vous ferons découvrir le magnifique site boisé du resto, de la commune de Pontivy. Le programme de la journée est complet, pour occuper la journée. Vous aurez le choix entre deux trails, sur des parcours vallonnés de 12kms et 20kms. Profitez également des deux randonnées pédestres, de 8km et 15kms, en départ libre, pour découvrir de nouveaux chemins. Challengez-vous sur une marche nordique de 15kms chronométrée avec classement. Enfin, le départ du cross régional sera donné à 14h00.',
+    featured_image: ''
   }
 ]

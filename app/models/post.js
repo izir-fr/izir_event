1b5118794552adfaf3f5026640bc7486476e5b3c
jn-prod <nicolas.jouanno@gmail.com>
2018-11-23 12:01:36 +0100

    newsfeed db init

---
+++ app/models/post.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+// Event Schema
+var postSchema = mongoose.Schema({
+  titre: { type: String, required: true },
+  short_text: { type: String },
+  long_text: { type: String },
+  youtube_id: { type: String },
+  featured_image: { type: String },
+  event_id: { type: Schema.ObjectId, ref: 'Event' },
+  featured: { type: Boolean, default: false },
+  blog_poost: { type: Boolean, default: false },
+  published_date: { type: Date },
+  created_at: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('Post', postSchema)

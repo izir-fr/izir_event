e9f5296087ce34a0d95e2bddb8a004b449b6f63d
jn-prod <nicolas.jouanno@gmail.com>
2018-12-04 11:54:35 +0100

    Update chronometrage.js

--- custom_modules/app/chronometrage.js
+++ custom_modules/app/chronometrage.js
@@ -181,7 +181,7 @@ var exportModules = {
       'ETAT': '',
       'PAYS': '',
       'EMAIL': val.participant.email,
-      'TEL': val.phone,
+      'TEL': val.participant.phone,
       'SEXE': val.participant.sex,
       'NUMERO': '',
       'HANDICAP': '',
